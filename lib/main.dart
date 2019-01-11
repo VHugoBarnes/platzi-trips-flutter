4810f12b0e42fdec6d56f203eebc0e96d6bc7541
VHugoBarnes <hugovg799@gmail.com>
2019-01-11 12:37:28 -0600

    something

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Platzi Trips',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,7 +20,24 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+
+          appBar: AppBar(
+
+              title: Text("Hola, mundo :p", textAlign: TextAlign.center),
+
+          ),
+          body: Center(
+
+              child: Text("Hola, mundo"),
+
+          ),
+        //   bottomNavigationBar: ,
+        //   floatingActionButton: ,
+        //   floatingActionButtonLocation: ,
+
+      )
+      //MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
