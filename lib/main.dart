5e3f8e2a435479db278a2a46635656cd42f41075
VHugoBarnes <hugovg799@gmail.com>
2019-01-11 18:42:49 -0600

    Challenge Complete

--- lib/main.dart
+++ lib/main.dart
@@ -4,40 +4,46 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Platzi Trips',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
       home: Scaffold(
-
           appBar: AppBar(
-
-              title: Text("Hola, mundo :p", textAlign: TextAlign.center),
-
+            leading: Icon(Icons.arrow_back),
+            title: Text("Share", textAlign: TextAlign.center),
           ),
+
           body: Center(
-
-              child: Text("Hola, mundo"),
-
+              child: Stack(
+                children: <Widget>[
+                  Image.network("https://www.setaswall.com/wp-content/uploads/2017/04/Material-Wallpaper-4-1920x1080-768x432.png",
+                  fit: BoxFit.fill,
+                  height: double.maxFinite,
+                  ),
+                  Center(
+                    child: Container(
+                      height: 70.0,
+                      color: Colors.black45,
+                      child: Center(
+                        child: Text(
+                          "XD",
+                          style: TextStyle(fontSize: 24, color: Colors.white),
+                        ),
+                      ),
+                  ),
+                  )
+                ],
+              ),
           ),
+
         //   bottomNavigationBar: ,
         //   floatingActionButton: ,
         //   floatingActionButtonLocation: ,
 
-      )
+      ) 
     );
   }
 } 
